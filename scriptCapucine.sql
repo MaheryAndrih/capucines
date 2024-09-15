9be16bbbfc56d82bde980c98fadf95f4b617b875
Maela1698 <119570144+Maela1698@users.noreply.github.com>
2024-09-15 22:21:28 +0300

    script done

--- scriptCapucine.sql
+++ scriptCapucine.sql
@@ -1,18 +1,18 @@
-create table user(
+create table utilisateur(
     id char(9) primary key,
     username varchar(40) not null,
     password varchar(40) unique not null
 );
 
-create sequence user_seq 
+create sequence utilisateur_seq 
     start with 1
     increment by 1
     minvalue 1;
 
-insert into user values
-('USR' || RIGHT('000000' || nextval('user_seq'), 6),'andry','andry'),
-('USR' || RIGHT('000000' || nextval('user_seq'), 6),'ravo','ravo'),
-('USR' || RIGHT('000000' || nextval('user_seq'), 6),'rija','rija');
+insert into utilisateur values
+('USR' || RIGHT('000000' || nextval('utilisateur_seq'), 6),'andry','andry'),
+('USR' || RIGHT('000000' || nextval('utilisateur_seq'), 6),'ravo','ravo'),
+('USR' || RIGHT('000000' || nextval('utilisateur_seq'), 6),'rija','rija');
 
 create table epreuve(
     id char(9) primary key,
@@ -40,7 +40,7 @@ insert into epreuve values
 create table classe(
     id char(9) primary key,
     nom_classe varchar(30) not null,
-    code_classe varchar(5) not null,
+    code_classe varchar(10) not null,
     unique(nom_classe,code_classe)
 );
 
@@ -102,7 +102,7 @@ create table eleve(
     id char(9) primary key,
     nom varchar(30) not null,
     prenom varchar(30) not null,
-    dtn date 
+    dtn date check (dtn <= NOW())
 );
 
 create sequence eleve_seq 
@@ -128,34 +128,20 @@ INSERT INTO eleve( id, nom, prenom, dtn ) VALUES
 ('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANDRASANA', 'Nantenaina', '2020-12-01'),
 ('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMANANA', 'Solofa', '2021-12-01'),
 ('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RASOLOFOZAFY', 'Mialy', '2022-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Nantenaina', '2023-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMIADANA', 'Joronavalona', '2024-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RATSIMBAZAFY', 'Mialy', '2025-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAKOTOMANANA', 'Nantenaina', '2026-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Solofa', '2027-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMAHARO', 'Herilala', '2028-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RASOLOMANANA', 'Fidisoa', '2029-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAKOTOSON', 'Tiana', '2030-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANDRASANA', 'Nantenaina', '2031-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMANANA', 'Solofa', '2032-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RASOLOFOZAFY', 'Mialy', '2033-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Nantenaina', '2034-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMIADANA', 'Joronavalona', '2035-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RATSIMBAZAFY', 'Mialy', '2036-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAKOTOMANANA', 'Nantenaina', '2037-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Solofa', '2038-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMAHARO', 'Herilala', '2039-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RASOLOMANANA', 'Fidisoa', '2040-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAKOTOSON', 'Tiana', '2041-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANDRASANA', 'Nantenaina', '2042-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMANANA', 'Solofa', '2043-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RASOLOFOZAFY', 'Mialy', '2044-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Nantenaina', '2045-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMIADANA', 'Joronavalona', '2046-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RATSIMBAZAFY', 'Mialy', '2047-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAKOTOMANANA', 'Nantenaina', '2048-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Solofa', '2049-12-01'),
-('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMAHARO', 'Herilala', '2050-12-01');
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Nantenaina', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMIADANA', 'Joronavalona', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RATSIMBAZAFY', 'Mialy', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAKOTOMANANA', 'Nantenaina', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Solofa', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMAHARO', 'Herilala', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RASOLOMANANA', 'Fidisoa', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAKOTOSON', 'Tiana', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANDRASANA', 'Nantenaina', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMANANA', 'Solofa', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RASOLOFOZAFY', 'Mialy', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RAMANANTSOA', 'Nantenaina', '2023-09-14'),
+('ELV' || RIGHT('000000' || nextval('eleve_seq'), 6), 'RANDRIAMIADANA', 'Joronavalona', '2023-09-14');
+
 
 create table classe_eleve(
     id_classe char(9) references classe(id),
@@ -199,40 +185,83 @@ create table classe_matiere_coefficient(
     id_classe char(9) references classe(id) not null,
     id_matiere char(9) references matiere(id) not null,
     coefficient int not null check(coefficient >= 1),
-    unique(id_classe,id_matiere),
+    unique(id_classe,id_matiere)
 );
 
-INSERT INTO classe_matiere_coefficient (id_classe, id_matiere, coefficient) VALUES
+INSERT INTO classe_matiere_coefficient(id_classe, id_matiere, coefficient) VALUES
 ('CLS000001', 'MAT000001', 3),
 ('CLS000001', 'MAT000002', 4),
 ('CLS000001', 'MAT000003', 2),
 ('CLS000001', 'MAT000004', 5),
 ('CLS000001', 'MAT000005', 3),
-('CLS000002', 'MAT000001', 4),
+('CLS000001', 'MAT000006', 2),
+('CLS000001', 'MAT000007', 4),
+('CLS000001', 'MAT000008', 3),
+('CLS000001', 'MAT000009', 5),
+('CLS000001', 'MAT000010', 1),
+('CLS000001', 'MAT000011', 4),
+('CLS000001', 'MAT000012', 2),
+('CLS000002', 'MAT000001', 2),
+('CLS000002', 'MAT000002', 3),
+('CLS000002', 'MAT000003', 5),
+('CLS000002', 'MAT000004', 4),
+('CLS000002', 'MAT000005', 3),
 ('CLS000002', 'MAT000006', 2),
-('CLS000002', 'MAT000007', 3),
-('CLS000002', 'MAT000008', 5),
+('CLS000002', 'MAT000007', 5),
+('CLS000002', 'MAT000008', 4),
 ('CLS000002', 'MAT000009', 3),
+('CLS000002', 'MAT000010', 2),
+('CLS000002', 'MAT000011', 1),
+('CLS000002', 'MAT000012', 4),
+('CLS000003', 'MAT000001', 5),
 ('CLS000003', 'MAT000002', 3),
+('CLS000003', 'MAT000003', 2),
 ('CLS000003', 'MAT000004', 4),
-('CLS000003', 'MAT000005', 5),
-('CLS000003', 'MAT000010', 2),
-('CLS000003', 'MAT000011', 4),
+('CLS000003', 'MAT000005', 1),
+('CLS000003', 'MAT000006', 3),
+('CLS000003', 'MAT000007', 5),
+('CLS000003', 'MAT000008', 4),
+('CLS000003', 'MAT000009', 2),
+('CLS000003', 'MAT000010', 5),
+('CLS000003', 'MAT000011', 3),
+('CLS000003', 'MAT000012', 4),
+('CLS000004', 'MAT000001', 3),
+('CLS000004', 'MAT000002', 5),
 ('CLS000004', 'MAT000003', 2),
+('CLS000004', 'MAT000004', 1),
+('CLS000004', 'MAT000005', 4),
 ('CLS000004', 'MAT000006', 3),
+('CLS000004', 'MAT000007', 5),
 ('CLS000004', 'MAT000008', 4),
-('CLS000004', 'MAT000010', 5),
-('CLS000004', 'MAT000012', 3),
-('CLS000005', 'MAT000001', 3),
-('CLS000005', 'MAT000005', 4),
+('CLS000004', 'MAT000009', 2),
+('CLS000004', 'MAT000010', 3),
+('CLS000004', 'MAT000011', 1),
+('CLS000004', 'MAT000012', 5),
+('CLS000005', 'MAT000001', 4),
+('CLS000005', 'MAT000002', 2),
+('CLS000005', 'MAT000003', 3),
+('CLS000005', 'MAT000004', 5),
+('CLS000005', 'MAT000005', 1),
+('CLS000005', 'MAT000006', 4),
 ('CLS000005', 'MAT000007', 2),
-('CLS000005', 'MAT000009', 5),
-('CLS000005', 'MAT000011', 3),
-('CLS000006', 'MAT000002', 4),
-('CLS000006', 'MAT000004', 3),
+('CLS000005', 'MAT000008', 5),
+('CLS000005', 'MAT000009', 3),
+('CLS000005', 'MAT000010', 1),
+('CLS000005', 'MAT000011', 2),
+('CLS000005', 'MAT000012', 4),
+('CLS000006', 'MAT000001', 5),
+('CLS000006', 'MAT000002', 3),
+('CLS000006', 'MAT000003', 4),
+('CLS000006', 'MAT000004', 2),
+('CLS000006', 'MAT000005', 1),
 ('CLS000006', 'MAT000006', 5),
-('CLS000006', 'MAT000008', 2),
-('CLS000006', 'MAT000012', 3);
+('CLS000006', 'MAT000007', 3),
+('CLS000006', 'MAT000008', 4),
+('CLS000006', 'MAT000009', 5),
+('CLS000006', 'MAT000010', 2),
+('CLS000006', 'MAT000011', 3),
+('CLS000006', 'MAT000012', 4);
+
 
 CREATE TABLE classe_epreuve (
     id_classe CHAR(9) REFERENCES classe(id) NOT NULL,
@@ -240,9 +269,7 @@ CREATE TABLE classe_epreuve (
     UNIQUE(id_classe, id_epreuve)
 );
 
--- Insertion des données de test
 INSERT INTO classe_epreuve (id_classe, id_epreuve) VALUES
--- Classe 1
 ('CLS000001', 'EPR000001'),
 ('CLS000001', 'EPR000002'),
 ('CLS000001', 'EPR000003'),
@@ -298,7 +325,59 @@ INSERT INTO classe_epreuve (id_classe, id_epreuve) VALUES
 ('CLS000006', 'EPR000008'),
 ('CLS000006', 'EPR000009');
 
+create table note(
+    id char(9) primary key,
+    id_classe char(9) references classe(id) not null,
+    id_eleve char(9) references eleve(id) not null,
+    id_epreuve char(9) references epreuve(id) not null,
+    id_matiere char(9) references matiere(id) not null,
+    note double precision check (note >=0 and note <= 20),
+    unique(id_classe,id_eleve,id_epreuve,id_matiere)
+);
 
+create sequence note_seq
+    start with 1
+    increment by 1
+    minvalue 1;
+
+INSERT INTO note (id, id_classe, id_eleve, id_epreuve, id_matiere, note) VALUES
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000001', 'ELV000001', 'EPR000001', 'MAT000001', 15.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000001', 'ELV000001', 'EPR000002', 'MAT000002', 16.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000001', 'ELV000002', 'EPR000003', 'MAT000003', 12.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000001', 'ELV000003', 'EPR000004', 'MAT000004', 18.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000001', 'ELV000004', 'EPR000005', 'MAT000005', 14.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000001', 'ELV000005', 'EPR000006', 'MAT000006', 13.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000002', 'ELV000006', 'EPR000001', 'MAT000007', 10.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000002', 'ELV000007', 'EPR000002', 'MAT000008', 17.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000002', 'ELV000008', 'EPR000003', 'MAT000009', 19.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000002', 'ELV000009', 'EPR000004', 'MAT000010', 16.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000002', 'ELV000010', 'EPR000005', 'MAT000011', 13.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000003', 'ELV000011', 'EPR000001', 'MAT000012', 9.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000003', 'ELV000012', 'EPR000002', 'MAT000001', 8.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000003', 'ELV000013', 'EPR000003', 'MAT000002', 14.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000003', 'ELV000014', 'EPR000004', 'MAT000003', 19.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000003', 'ELV000015', 'EPR000005', 'MAT000004', 17.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000004', 'ELV000016', 'EPR000006', 'MAT000005', 10.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000004', 'ELV000017', 'EPR000007', 'MAT000006', 11.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000004', 'ELV000018', 'EPR000008', 'MAT000007', 12.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000004', 'ELV000019', 'EPR000009', 'MAT000008', 17.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000004', 'ELV000020', 'EPR000001', 'MAT000009', 9.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000005', 'ELV000021', 'EPR000002', 'MAT000010', 10.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000005', 'ELV000022', 'EPR000003', 'MAT000011', 14.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000005', 'ELV000023', 'EPR000004', 'MAT000012', 18.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000005', 'ELV000024', 'EPR000005', 'MAT000001', 16.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000005', 'ELV000025', 'EPR000006', 'MAT000002', 11.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000006', 'ELV000026', 'EPR000007', 'MAT000003', 13.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000006', 'ELV000027', 'EPR000008', 'MAT000004', 15.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000006', 'ELV000028', 'EPR000009', 'MAT000005', 18.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000006', 'ELV000029', 'EPR000001', 'MAT000006', 16.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000006', 'ELV000030', 'EPR000002', 'MAT000007', 19.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000001', 'ELV000001', 'EPR000003', 'MAT000002', 14.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000002', 'ELV000008', 'EPR000004', 'MAT000011', 17.5),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000003', 'ELV000014', 'EPR000005', 'MAT000009', 18.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000004', 'ELV000018', 'EPR000006', 'MAT000010', 16.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000005', 'ELV000023', 'EPR000007', 'MAT000012', 19.0),
+('NTE' || RIGHT('000000' || nextval('note_seq'), 6), 'CLS000006', 'ELV000030', 'EPR000009', 'MAT000003', 13.0);
 
 
 
